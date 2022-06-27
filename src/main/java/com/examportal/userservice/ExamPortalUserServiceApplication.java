eb4de58790db5edf3a93d800cbd1cfda3dec2d44
Sanket Panda <sanketpanda55.sp@gmail.com>
2022-06-27 07:08:39 +0530

    Exam Portal UserService Project

---
+++ src/main/java/com/examportal/userservice/ExamPortalUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.examportal.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPortalUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamPortalUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examportal/userservice/ExamPortalUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examportal.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPortalUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
